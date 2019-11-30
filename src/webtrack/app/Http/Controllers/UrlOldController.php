641d37966c25ec30b95cbf81959075e0282b903a
anakin <livio.lopes@armateus.com.br>
2019-11-30 10:59:43 -0300

    desenvolvimento front

---
+++ src/webtrack/app/Http/Controllers/UrlOldController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class UrlOldController extends Controller
+{
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function getUrl(Request $request){
+
+//        $url = $request->url;
+//        if (filter_var($url, FILTER_VALIDATE_URL)) {
+//            echo("$url is a valid URL");
+//        } else {
+//            echo("$url is not a valid URL");
+//        }
+////        $validatedData = $request->validate([
+////            'url' => 'required|unique:posts|max:255',
+////        ]);
+///
+        Log::info("Acessei a rota: ");
+        return [$request->url];
+
+    }
+
+}
